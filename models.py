a65b9a3db4f820ec9a480722abe73b7610b2902f
dev-com2020 <kwp@tlen.pl>
2023-03-25 11:41:35 +0100

    25.03 g. 11:41

--- models.py
+++ models.py
@@ -6,14 +6,21 @@ def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s).lower()
 
-class Entry(db.Model):
 
+entry_tags = db.Table('entry_tags',
+                      db.Column('entry_id', db.Integer, db.ForeignKey('entry.id')),
+                      db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')))
+
+
+class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     slug = db.Column(db.String(100), unique=True)
     content = db.Column(db.Text)
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+    tags = db.relationship('Tag', secondary=entry_tags, backref=db.backref('entries', lazy='dynamic'))
 
     def __init__(self, *args, **kwargs):
         super(Entry, self).__init__(*args, **kwargs)
@@ -26,3 +33,16 @@ class Entry(db.Model):
 
     def __repr__(self):
         return '<Entry: {}>'.format(self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag: {}>'.format(self.name)
