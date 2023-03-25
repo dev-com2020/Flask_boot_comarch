d23909f13f1f2d0198b736fd444ef8aa52eb4e69
dev-com2020 <kwp@tlen.pl>
2023-03-25 13:09:23 +0100

    25.03 g. 13:09

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-# from flask_migrate import Migrate, MigrateCommand
-# from flask_script import Manager
+from flask_migrate import Migrate
+
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
@@ -8,8 +8,6 @@ from config import Configuration
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
-# manager = Manager(app)
-# manager.add_command('db', MigrateCommand)
 
