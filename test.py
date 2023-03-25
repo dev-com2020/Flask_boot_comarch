f1f78874806661ce5ff8b77645df0a1c75fba33e
dev-com2020 <kwp@tlen.pl>
2023-03-25 09:05:27 +0100

    25.03 g. 09:05

--- test.py
+++ test.py
@@ -3,9 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
