0f362facb2381e662a5df34c7e1e7f94346bc07f
dev-com2020 <kwp@tlen.pl>
2023-03-25 11:20:35 +0100

    25.03 g. 11:20

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ from config import Configuration
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
+
---
+++ create_db.py
@@ -0,0 +1,8 @@
+from app import app
+from main import db
+import os, sys
+
+sys.path.append(os.getcwd())
+
+with app.app_context():
+    db.create_all()
