b845e9f1f3f4173c1aa83e6ccf842f1390eee454
dev-com2020 <kwp@tlen.pl>
2023-03-25 14:44:51 +0100

    25.03 g. 14:44

--- dane_do_bazy.py
+++ dane_do_bazy.py
@@ -1,8 +1,18 @@
+# import sqlite3
+# conn = sqlite3.connect('blog.db')
+# c = conn.cursor()
+# dane = c.execute('SELECT * FROM tag')
+# # c.execute('INSERT INTO tag (name,slug) VALUES ("C++","c++")')
+# # conn.commit()
+#
+# print(dane.fetchall())
+
+
 import sqlite3
 conn = sqlite3.connect('blog.db')
 c = conn.cursor()
-dane = c.execute('SELECT * FROM tag')
-# c.execute('INSERT INTO tag (name,slug) VALUES ("C++","c++")')
-# conn.commit()
+dane = c.execute('SELECT * FROM entry')
+c.execute('INSERT INTO entry (title,slug,content,status) VALUES ("Kod dla C++","kod-dl-c++","Kod dla C++...", 1)')
+conn.commit()
 
 print(dane.fetchall())
